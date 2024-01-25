6efa76283eace0c89de53c3f5790272afa90320a
pedromarinho262@gmail.com <pedromarinho262@gmail.com>
2024-01-24 21:42:16 -0300

    Solicitações à API OpenWeatherMap e manipulação com JavaSricpt
    Agora, a cidade que é digitada no 'input' é passada para a URL da API que consegue trazer as informações climáticas da região.

---
+++ main.js
@@ -0,0 +1,21 @@
+const apiKey = "c5edb2eef0934fbd06c096bd7c3ee89c";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    var data = await response.json();
+
+    console.log(data);
+
+    document.querySelector(".city"). innerText = data.name;
+    document.querySelector(".temp").innerText = Math.round(data.main.temp) + "ºC";
+    document.querySelector(".humidity").innerText = data.main.humidity + "%";
+    document.querySelector(".wind").innerText = data.wind.speed + " km/h";
+}
+
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+})
