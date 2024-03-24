56f5f3609fabd3d236bd5f6b2560b678412e3e76
pedromarinho262@gmail.com <pedromarinho262@gmail.com>
2024-03-24 08:06:01 -0300

    Inserção de imagens e tratativas de erros

--- main.js
+++ main.js
@@ -3,17 +3,44 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon")
 
 async function checkWeather(city) {
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-    var data = await response.json();
 
-    console.log(data);
+    if (response.status == 404) {
+        document.querySelector(".error").style.display = 'block';
+        document.querySelector(".weather").style.display = 'none';
+    }
+    else {
+        var data = await response.json();
 
-    document.querySelector(".city"). innerText = data.name;
-    document.querySelector(".temp").innerText = Math.round(data.main.temp) + "ºC";
-    document.querySelector(".humidity").innerText = data.main.humidity + "%";
-    document.querySelector(".wind").innerText = data.wind.speed + " km/h";
+        document.querySelector(".city").innerText = data.name;
+        document.querySelector(".temp").innerText = Math.round(data.main.temp) + "ºC";
+        document.querySelector(".humidity").innerText = data.main.humidity + "%";
+        document.querySelector(".wind").innerText = data.wind.speed + " km/h";
+
+        if (data.weather[0].main == 'Clouds') {
+            weatherIcon.src = 'images/clouds.png';
+        }
+        else if (data.weather[0].main == 'Clear') {
+            weatherIcon.src = 'images/clear.png';
+        }
+        else if (data.weather[0].main == 'Rain') {
+            weatherIcon.src = 'images/rain.png';
+        }
+        else if (data.weather[0].main == 'Drizzle') {
+            weatherIcon.src = 'images/drizzle.png';
+        }
+        else if (data.weather[0].main == 'Snow') {
+            weatherIcon.src = 'images/snow.png';
+        }
+        else if (data.weather[0].main == 'Mist') {
+            weatherIcon.src = 'images/mist.png';
+        }
+
+        document.querySelector(".weather").style.display = 'block';
+    }
 }
 
 searchBtn.addEventListener("click", () => {
